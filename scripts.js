cf5c4d7981cfcaeab03e8e86eabedd2a12243adf
Ross <blindross@gmail.com>
2023-03-24 10:22:41 +0000

    javascript type up

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// const is constant variable, let is not a constant variable 
+// 
+const  heading = document.getElementById("heading");
+
+// console log is a method that outputs a message to the web console (highlight or in Inspect)
+console.log(heading);
+
+// variable assigned value in quote is string
+const  myName = "Ross";
+
+// variable assigned value in number, no quotes
+let myAge = 40;
+
+// variable assigned value is a boolean, true or false and in no quotes
+let isAlive = true;
+
+// variable assigned value is array, is a line of code that can consists a ine of string. [] Braces.
+const myPets = ["Dog", "Cat", "Goldfish"]
+
+// variable assigned value is an object, is a standalone entity, with properties and type. {} Braces.
+const cup = {
+    color: "Blue",
+    design: "mosiac",
+    madeOfChina: true,
+    weightInGrams: 100,
+}
